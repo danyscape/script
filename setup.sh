998a9259fafc7602d6bf601682f647bbdbcec9b3
dany <adamzulkey@gmail.com>
2021-12-13 17:25:06 +0800

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,7 @@
+cd /
+sudo mkdir idk
+sudo chown -R adamzulkey@gmail.com: idk
+cd idk
+wget https://raw.githubusercontent.com/akhilnarang/scripts/master/setup/android_build_env.sh && bash android*
+git config --global user.email adamzulkey@gmail.com
+git config --global user.name Danyscape
