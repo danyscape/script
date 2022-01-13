5d1cdcceeb80307a61686adaa8f6647ee5c57270
dany <adamzulkey@gmail.com>
2022-01-13 21:27:49 +0800

    fix derp

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 cd /
 sudo mkdir idk
-sudo chown -R adamzulkey@gmail.com: idk
+sudo chown -R adamzulkey: idk
 cd idk
 wget https://raw.githubusercontent.com/akhilnarang/scripts/master/setup/android_build_env.sh && bash android*
 git config --global user.email adamzulkey@gmail.com
